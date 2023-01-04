490c4a6b8b7b0a3f564f824b5db237f8a761f976
John Fletcher <john@bunbury28.plus.com>
2023-01-04 22:50:06 +0000

    Update sweepIOAbs_moustache.ino
    Use new code from the library

--- sweepIOAbs_moustache/sweepIOAbs_moustache.ino
+++ sweepIOAbs_moustache/sweepIOAbs_moustache.ino
@@ -22,21 +22,7 @@
 #include <ExecWithParameter.h>
 
 #include <moustache.h>
-
-// This returns the size of a moustache_variable_t array and also of an array of arrays.
-// It can be used to control output from rendering the array of arrays
-template <typename T, size_t n>
-size_t moustache_size(T (&values)[n])
-{
-  return n;
-}
-
-template <typename T, typename V, size_t n>
-void moustache_value(T (&values)[n], size_t i, const V &v)
-{
-  if (i < n) values[i].value = String(v);
-  //return n;
-}
+#include <moustache_code.h>
 
 // Define the formats to be used to output of position.
 const char *position = "moveServo reaches {{pos}}";
